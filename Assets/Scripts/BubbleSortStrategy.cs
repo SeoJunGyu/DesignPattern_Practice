0a1b0955ecc98061b855b0b3129cfada52f0fa18
서준규 <thddlwnsrb@naver.com>
2025-11-04 18:51:59 +0900

    버블 정렬만 추가

--- Assets/Scripts/BubbleSortStrategy.cs
+++ Assets/Scripts/BubbleSortStrategy.cs
@@ -5,6 +5,18 @@ public class BubbleSortStrategy<T> : ISortStrategy<T> where T : IComparable<T>
 {
     public void Sort(T[] array)
     {
-        throw new System.NotImplementedException();
+        for(int i = 0; i < array.Length - 1; i++)
+        {
+            for(int j = 0; j < array.Length - i - 1; j++)
+            {
+                if(array[j].CompareTo(array[j + 1]) > 0)
+                {
+                    // Swap array[j] and array[j + 1]
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/InsertionSortStrategy.cs
+++ Assets/Scripts/InsertionSortStrategy.cs
@@ -1,10 +1,33 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InsertionSortStrategy<T> : ISortStrategy<T> where T : IComparable<T>
 {
+    private readonly IComparer<T> comparer;
+
+    public InsertionSortStrategy()
+    {
+        comparer = Comparer<T>.Default;
+    }
+
+    public InsertionSortStrategy(IComparer<T> customComparer)
+    {
+        comparer = customComparer;
+    }
+
     public void Sort(T[] array)
     {
-        throw new System.NotImplementedException();
+        //2 4 3 1 0
+        //
+        for(int i = 1; i < array.Length; i++)
+        {
+            if(comparer.Compare(array[i], array[i - 1]) < 0)
+            {
+                T tmp = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = tmp;
+            }
+        }
     }
 }
